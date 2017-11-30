511f25a7c669e4c0ca4222a1a1dc7de3094eab70
frenandi <luis.juarez@autoweb.com>
2017-11-30 08:12:22 -0600

    append to the table + adding ids to the tables + function to retrieve data

--- ui/scripts/app/BudgetOperations.js
+++ ui/scripts/app/BudgetOperations.js
@@ -1,12 +1,35 @@
-var app = (function(){
-    var addNewItem = function(){
-        var incomeOrExpense = document.getElementById("operationTypeSelect");
-        var itemText = document.getElementById("operationTextInput");
-        var itemValue = document.getElementById("operationValueInput");
-
-        console.log("incomeOrExpense= "+ incomeOrExpense + "o= "+ itemText + "o= "+ itemValue);
+var app = (function () {
+    var addNewItem = function () {
+        var item = getItemValues();
+        addItemToTable(item);        
+        console.log("incomeOrExpense= " + item.itemTypeValue + "o = " + item.itemText + "o = " + item.itemValue);
     };
+
+    var getItemValues = function () {
+        var itemTypeSelect = document.getElementById("operationTypeSelect");
+        var item = {
+            itemTypeValue: itemTypeSelect.options[itemTypeSelect.selectedIndex].value == "+" ? "incomesTable" : "expensesTable",
+            itemText: document.getElementById("operationTextInput").value,
+            itemValue: document.getElementById("operationValueInput").value
+        }
+        return item;
+    };
+
+    var addItemToTable = function (item) {       
+        var table = document.getElementById(item.itemTypeValue);
+        var newRow = table.insertRow(-1);
+        var newCell = newRow.insertCell(-1);
+        var newCell2 = newRow.insertCell(-1);
+
+        var newText = document.createTextNode(item.itemText);
+        newCell.appendChild(newText);
+
+        var newText = document.createTextNode(item.itemValue);
+        newCell2.appendChild(newText);
+    };
+
+
     return {
         addNewItem: addNewItem
     }
-} ());
+}());
